8e9dcd9d2a2a550d186455d1f8eeaa207a847260
Tobotobo <tobotobo.channel@gmail.com>
2023-05-04 14:13:30 +0900

    初回

---
+++ README.md
@@ -0,0 +1,4 @@
+# java_spring_jpa_example1
+
+
+https://spring.pleiades.io/guides/gs/accessing-data-jpa/
---
+++ demo/src/test/java/com/example/demo/repository/CustomerRepositoryTest.java
@@ -0,0 +1,65 @@
+package com.example.demo.repository;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.example.demo.entity.Customer;
+
+// @ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class CustomerRepositoryTest {
+    
+    private static final Logger log = LoggerFactory.getLogger(CustomerRepositoryTest.class);
+
+    @Autowired
+    CustomerRepository repository;
+
+    @Test
+    @DisplayName("成功するで")
+    @Tag("manual")
+    public void test1() {
+        log.info("CustomerRepositoryTest - test1");
+
+        // save a few customers
+        repository.save(new Customer("Jack", "Bauer"));
+        repository.save(new Customer("Chloe", "O'Brian"));
+        repository.save(new Customer("Kim", "Bauer"));
+        repository.save(new Customer("David", "Palmer"));
+        repository.save(new Customer("Michelle", "Dessler"));
+
+        // fetch all customers
+        log.info("Customers found with findAll():");
+        log.info("-------------------------------");
+        for (Customer customer : repository.findAll()) {
+            log.info(customer.toString());
+        }
+        log.info("");
+
+        // fetch an individual customer by ID
+        Customer customer = repository.findById(1L);
+        log.info("Customer found with findById(1L):");
+        log.info("--------------------------------");
+        log.info(customer.toString());
+        log.info("");
+
+        // fetch customers by last name
+        log.info("Customer found with findByLastName('Bauer'):");
+        log.info("--------------------------------------------");
+        repository.findByLastName("Bauer").forEach(bauer -> {
+            log.info(bauer.toString());
+        });
+        // for (Customer bauer : repository.findByLastName("Bauer")) {
+        //  log.info(bauer.toString());
+        // }
+        log.info("");
+    }
+
+
+}
